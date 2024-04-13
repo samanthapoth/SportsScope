bf175315d2e1030446c454d6d5cf6626e6a2f4e4
hleaders <80873733+hleaders@users.noreply.github.com>
2024-04-13 10:49:40 -0500

    adding models and database folders

---
+++ FlowHub/models/game.py
@@ -0,0 +1,14 @@
+from typing import Optional
+from beanie import Document, Indexed
+from pydantic import BaseModel, Field
+from team import Team
+
+class Game(Document):
+    home_team: Team
+    away_team: Team
+    date: str
+    home_team_score: int = Field(default=0)
+    away_team_score: int = Field(default=0)
+
+    class Settings:
+        name = "game"
---
+++ FlowHub/models/team.py
@@ -0,0 +1,24 @@
+from typing import Optional
+from beanie import Document, Indexed
+from pydantic import BaseModel, Field,  ConfigDict
+
+class Team(Document):
+    name: str = ""
+    logo: str = ""
+    players: list[str] = []
+    location: str = ""
+
+    model_config = ConfigDict(
+        json_schema_extra={
+            "example": {
+                "name": "Project 1 Presentation",
+                "logo": "https://placehold.co/600x400?text=Project+1+Presentation",
+                "players": [
+                    "Henry Leaders",
+                    "Taylor Micetich",
+                    "Samantha Pothitakis",
+                ],
+                "location": "Iowa City",
+            }
+        }
+    )
