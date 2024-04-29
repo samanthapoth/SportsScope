f2e9a244805ec517d265971381638826084e0e16
samanthapoth <90707077+samanthapoth@users.noreply.github.com>
2024-04-28 19:44:21 -0500

    CORRECT SPORTSSCOPE APP

---
+++ sportsScope/models/game.py
@@ -0,0 +1,32 @@
+from typing import Optional
+from beanie import Document, Indexed
+from pydantic import BaseModel, Field
+from models.team import Team
+
+class Game(Document):
+    name: str
+    location: str
+    home_team: Team
+    away_team: Team
+    date: str
+    time: str
+    played: bool = Field(default=False)
+    home_team_score: Optional[int] = Field(default=0)
+    away_team_score: Optional[int] = Field(default=0)
+
+    class Settings:
+        name = "game"
+
+class GameUpdate(BaseModel):
+    name: str
+    location: str
+    home_team: Team
+    away_team: Team
+    date: str
+    time: str
+    played: bool = Field(default=False)
+    home_team_score: Optional[int] = Field(default=0)
+    away_team_score: Optional[int] = Field(default=0)
+
+    class Settings:
+        name = "game"
