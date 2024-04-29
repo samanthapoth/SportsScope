96685f6c869b341c61d23fd8afae3f282fbfbdec
samanthapoth <90707077+samanthapoth@users.noreply.github.com>
2024-04-28 20:56:36 -0500

    Delete sportsScope directory

--- sportsScope/src/app/teams/team.model.ts
+++
@@ -1,9 +0,0 @@
-import { Player } from './players/player.model'; // Ensure this path is correct
-
-export interface Team {
-  id: string;
-  name: string;
-  logo: string;
-  players: Player[]; // Use Player[] instead of string[]
-  location: string;
-}
