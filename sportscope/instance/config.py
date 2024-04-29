dc067a3f62ad1b672c16167bdd94708bd07b37b0
samanthapoth <90707077+samanthapoth@users.noreply.github.com>
2024-04-28 21:15:33 -0500

    Add files via upload

---
+++ sportscope/instance/config.py
@@ -0,0 +1,3 @@
+# Instance-specific configuration
+MONGO_URI = "mongodb://localhost:27017/flowhub"
+SECRET_KEY = 'your_secret_key_here'
---
+++ sportscope/models/team.py
@@ -0,0 +1,80 @@
+from typing import Optional
+from beanie import Document, Indexed
+from pydantic import BaseModel, Field,  ConfigDict
+from models.player import Player
+
+class Team(Document):
+    name: str = ""
+    logo: str = ""
+    players: list[Player] = []
+    location: str = ""
+
+    model_config = ConfigDict(
+        json_schema_extra={
+            "example": {
+                "name": "Project 1 Presentation",
+                "logo": "https://placehold.co/600x400?text=Project+1+Presentation",
+                "players": [
+                    {
+                    "name": "Henry Leaders",
+                    "age": 22,
+                    "position": "SF"
+                    },
+                    {
+                    "name": "Samantha",
+                    "age": 22,
+                    "position": "C"
+                    },
+                    {
+                    "name": "Alan",
+                    "age": 22,
+                    "position": "SG"
+                    },
+                    {
+                    "name": "Taylor",
+                    "age": 22,
+                    "position": "PF"
+                    },
+                ],
+                "location": "Iowa City"
+            }
+        }
+    )
+
+class TeamUpdate(BaseModel):
+    name: str = ""
+    logo: str = ""
+    players: list[Player] = []
+    location: str = ""
+
+    model_config = ConfigDict(
+        json_schema_extra={
+            "example": {
+                "name": "Project 1 Presentation",
+                "logo": "https://placehold.co/600x400?text=Project+1+Presentation",
+                "players": [
+                    {
+                    "name": "Henry Leaders",
+                    "age": 22,
+                    "position": "SF"
+                    },
+                    {
+                    "name": "Samantha",
+                    "age": 22,
+                    "position": "C"
+                    },
+                    {
+                    "name": "Alan",
+                    "age": 22,
+                    "position": "SG"
+                    },
+                    {
+                    "name": "Taylor",
+                    "age": 22,
+                    "position": "PF"
+                    },
+                ],
+                "location": "Iowa City"
+            }
+        }
+    )
