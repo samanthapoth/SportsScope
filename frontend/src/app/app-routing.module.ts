680f9f770fb4e2254dc216df04096d18a2a0f7f5
AlanCTB <115127598+AlanCTB@users.noreply.github.com>
2024-04-29 09:54:35 -0500

    Update app-routing.module.ts

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -4,6 +4,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { PlayersComponent } from './teams/players/players.component';
 import { TeamsComponent } from './teams/teams.component';
+import { GamesComponent } from './games/games.component';
 
 const routes: Routes = [
   { path: 'teams', component: TeamsComponent },
