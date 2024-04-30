7a0f220f8d51a9ce1d1a14db22d6a272c298e673
AlanCTB <115127598+AlanCTB@users.noreply.github.com>
2024-04-30 10:25:24 -0500

    Add files via upload

--- angular/app/games/models/game.ts
+++ angular/app/games/models/game.ts
@@ -4,11 +4,11 @@ export interface Game {
     _id?: string;  // Optional if not known yet (e.g., before creation)
     name: string;
     location: string;
-    homeTeam: string;
-    awayTeam: string;
+    home_team: string;
+    away_team: string;
     date: string;  // ISO string format for dates
     time: string;  // Could be a specific time format or ISO string
     played: boolean;
-    homeTeamScore?: number;  // Optional, may not be known at creation
-    awayTeamScore?: number;  // Optional, may not be known at creation
+    home_team_score?: number;  // Optional, may not be known at creation
+    away_team_score?: number;  // Optional, may not be known at creation
 }
